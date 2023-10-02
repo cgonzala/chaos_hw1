8eaab78ee82a967932f039b6d7cee880c5b5c365
Carlos <carlos.gonzaleza78@gmail.com>
2023-10-01 21:26:43 -0400

    Excercise 6 added

--- HW1_code.m
+++ HW1_code.m
@@ -1,4 +1,8 @@
-% HW 1
+##### Homework 1 #####
+##### Carlos Gonzalez & Aldair Desiderio #####
+
+
+##### Excercise 4 #####
 
 rvec = 1:0.01:4;  # Vector with r values
 
@@ -19,16 +23,47 @@ end
 xmat = xmat(101:end, :); # Delete first 100 rows of xmat
 
 # Plotting: for a given x value, plot all 400 iterates of x_n on top of each other
-figure;
-hold on;
-for t = 1:length(rvec)
-    plot(rvec(t) * ones((num_iterations-100), 1), xmat(:, t),
-    '.', 'MarkerSize', 2);
+
+#figure;
+#hold on;
+#for t = 1:length(rvec)
+    #plot(rvec(t) * ones((num_iterations-100), 1), xmat(:, t),
+    #'.', 'MarkerSize', 2);
     # You need this multiplication to create a column (num_iterations-100) tall
     # filled with rvec(t), so that you can stack xmat(:, t)
-end
-hold off;
+#end
+#hold off;
 
+#xlabel('r');
+#ylabel('x');
+
+###############################################################################
+
+##### Excercise 6 #####
+
+
+f = @(r, x) r * sin(pi*x);
+fprime = @(x, r) pi*r*cos(pi*x);
+iterations = 1000;
+rvec = 0:0.001:1;
+
+lyapunov_exp = zeros(size(rvec));
+
+for i = 1:length(rvec)
+    r = rvec(i);
+    x = rand(1);
+    l_sum = 0;
+
+    for j = 1:iterations
+        x = f(r, x);
+        l_sum = l_sum + log(abs(fprime(x,r)));
+    end
+    lyapunov_exp(i) = l_sum / iterations;
+end
+
+figure;
+plot(rvec, lyapunov_exp);
 xlabel('r');
-ylabel('x');
+ylabel('Lyapunov Exponent');
+
 
